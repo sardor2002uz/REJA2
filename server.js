5c64b9e0f60e06cc0028469c20af0ee0d6edf032
sardor <sardor020516@gmail.com>
2026-08-30 02:30:58 +0900

    REJA2

--- server.js
+++ server.js
@@ -2,6 +2,16 @@ console.log("web Serverni boshlash");
 const express = require("express");
 const app = express();
 const http = require("http");
+const fs = require("fs");
+
+let user;
+fs.readFile("database/user.json", "utf8", (err, data) => {
+    if(err) {
+        console.log("ERROR:", err);
+    } else {
+        user = JSON.parse(data)
+    }
+});
 
 // 1: Kirish code
 app.use(express.static("public"));
@@ -15,12 +25,27 @@ app.set("view engine", "ejs");
 
 
 // 4 Routing code
+
+app.post("/create-item", (req, res) => {
+    console.log(req);
+    res.json({test: "success"});
+});
+
+app.get('/author',(req, res) => {
+    res.render("author", {user: user});
+});
+
 app.get("/", function (req, res) {
-    res.end(`<h1>HELLO WORLD</h1>`);
+    res.render("harid");
 });
-app.get("/gift", function (req, res) {
-    res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-});
+
+
+//app.get("/", function (req, res) {
+//     res.end(`<h1>HELLO WORLD</h1>`);
+// });
+// app.get("/gift", function (req, res) {
+//     res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
+// });
 
 const server = http.createServer(app);
 let PORT = 3000;
