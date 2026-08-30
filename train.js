a7451a2e806e1c125cf8e0f5630c7d37c004845b
sardor <sardor020516@gmail.com>
2026-08-30 15:42:31 +0900

    feat: countLetter mitask

--- train.js
+++ train.js
@@ -1,13 +1,13 @@
-console.log ("Jack Ma maslahatlari");
-const list = [
-    "yaxshi talaba boling", // 0-20
-    "togri boshliq tanlang va koproq hato qiling", // 20-30
-    "uzingizga ishlashingizni boshlang", // 30-40
-    "siz kuchli bolgan narsalarni qiling", // 40-50
-    "yoshlarga investitsiya qiling", // 50-60
-    "endi dam oling, foydasi yoq endi", //60
+// console.log ("Jack Ma maslahatlari");
+// const list = [
+//     "yaxshi talaba boling", // 0-20
+//     "togri boshliq tanlang va koproq hato qiling", // 20-30
+//     "uzingizga ishlashingizni boshlang", // 30-40
+//     "siz kuchli bolgan narsalarni qiling", // 40-50
+//     "yoshlarga investitsiya qiling", // 50-60
+//     "endi dam oling, foydasi yoq endi", //60
 
-];
+// ];
 
 
 
@@ -58,27 +58,50 @@ const list = [
 
 // CALBACK function
 
-function maslahatBering(a, callback) {
-    if(typeof a !== 'number') callback ("Insert a number", null);
-    else if (a <=20) callback(null, list[0]);
-    else if (a > 20 && a <= 30) callback(null, list[1]);
-    else if (a > 30 && a <= 40) callback(null, list[2]);
-    else if (a > 40 && a <= 50) callback(null, list[3]);
-    else if (a > 50 && a <= 60) callback(null, list[4]);
-    else {
-        setInterval(function () {
-            callback(null, list[5]);
-        },) 
+// function maslahatBering(a, callback) {
+//     if(typeof a !== 'number') callback ("Insert a number", null);
+//     else if (a <=20) callback(null, list[0]);
+//     else if (a > 20 && a <= 30) callback(null, list[1]);
+//     else if (a > 30 && a <= 40) callback(null, list[2]);
+//     else if (a > 40 && a <= 50) callback(null, list[3]);
+//     else if (a > 50 && a <= 60) callback(null, list[4]);
+//     else {
+//         setInterval(function () {
+//             callback(null, list[5]);
+//         },) 
+//         }
+
+//         }
+
+
+//         console.log("passed here 0");
+//         maslahatBering(70,(err, data) => {
+//             if (err) console.log("ERROR:", err);
+//             else {
+//                 console.log(data);
+//             }
+//         });
+//         console.log("passed here 1");
+
+
+
+      function countLetter(letter, word) {
+    let count = 0;
+
+    for (let i = 0; i < word.length; i++) {
+        if (word[i] === letter) {
+            count++;
         }
+    }
 
-        }
+    return count;
+}
+
+countLetter("e", "engineer")
+
+console.log(countLetter("e", "engineer"));
 
 
-        console.log("passed here 0");
-        maslahatBering(70,(err, data) => {
-            if (err) console.log("ERROR:", err);
-            else {
-                console.log(data);
-            }
-        });
-        console.log("passed here 1");
+
+
+
