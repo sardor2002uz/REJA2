9b2d346bed62e5b86f276d924b788d8d6d7eba4f
sardor <sardor020516@gmail.com>
2026-09-02 01:07:44 +0900

    fix: portfolio fixed

--- app.js
+++ app.js
@@ -1,78 +1,72 @@
-console.log("web Serverni boshlash");
 const express = require("express");
+const fs = require("fs");
+
 const app = express();
 
+let user;
 
-
-
-// let user;
-// fs.readFile("database/user.json", "utf8", (err, data) => {
-//   if (err) {
-//     console.log("ERROR:", err);
-//   } else {
-//     user = JSON.parse(data);
-//   }
-// });
-
-// MongoDB choqirish
-const db = require("./server").db();
-
-// 1: Kirish code
-app.use(express.static("public"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// 2: Session code
-// 3: Views code
-app.set("views", "views");
-app.set("view engine", "ejs");
-
-// 4 Routing code
-
-app.post("/create-item", (req, res) => {
-  console.log("user entered /create-item");
-  const new_reja = req.body.reja;
-  db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
-   if(err) {
-    console.log(err);
-    res.end("something went wrong");
-   } else {
-    res.end("successfully added")
-   }
-  });
-  });
-
-// app.get("/author", (req, res) => {
-//   res.render("author", { user: user });
-// });
-
-app.get("/", function (req, res) {
-  console.log("user entered /");
-  db.collection("plans")
-  .find()
-  .toArray((err,data) => {
-    if (err) {
-      console.log(err);
-      res.end("something went wrong");
-    } else {
-      res.render("reja", { items: data} );
-      
-
-    }
-      
-  });
-    
+fs.readFile("database/user.json", "utf8", (err, data) => {
+  if (err) {
+    console.log("ERROR:", err);
+  } else {
+    user = JSON.parse(data);
+  }
 });
-  
 
-// app.get("/", function (req, res) {
-//   res.end(`<h1>HELLO WORLD</h1>`);
-// });
-// app.get("/gift", function (req, res) {
-//   res.end(`<h1>Siz sovgalar bolimidasiz</h1>`);
-// });
+module.exports = function (db) {
+  // 1: Middleware
 
-module.exports = app;
+  app.use(express.static("public"));
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
+  // 2: Views
 
+  app.set("views", "views");
+  app.set("view engine", "ejs");
 
+  // 3: Routes
+
+  app.post("/create-item", (req, res) => {
+    console.log("user entered /create-item");
+
+    const new_reja = req.body.reja;
+
+    db.collection("plans").insertOne(
+      { reja: new_reja },
+      (err, data) => {
+        if (err) {
+          console.log(err);
+          res.end("something went wrong");
+        } else {
+          res.end("successfully added");
+        }
+      }
+    );
+  });
+
+  app.get("/author", (req, res) => {
+    res.render("author", {
+      user: user,
+    });
+  });
+
+  app.get("/", (req, res) => {
+    console.log("user entered /");
+
+    db.collection("plans")
+      .find()
+      .toArray((err, data) => {
+        if (err) {
+          console.log(err);
+          res.end("something went wrong");
+        } else {
+          res.render("reja", {
+            items: data,
+          });
+        }
+      });
+  });
+
+  return app;
+};
--- server.js
+++ server.js
@@ -1,34 +1,35 @@
 const http = require("http");
 const mongodb = require("mongodb");
 
+const connectionString =
+  "mongodb+srv://sardor020516_db_user:Zx3MlVfZZ5T6gC5B@cluster0.5plyvsp.mongodb.net/Reja?appName=Cluster0";
 
-
-let db;
-const connectionString = "mongodb+srv://sardor020516_db_user:Zx3MlVfZZ5T6gC5B@cluster0.5plyvsp.mongodb.net/Reja?appName=Cluster0";
-
-mongodb.connect(connectionString,{
+mongodb.connect(
+  connectionString,
+  {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-}, (err, client) => {
-   if(err) console.log("ERROR on connection MongoDB");
-   else {
+  },
+  (err, client) => {
+    if (err) {
+      console.log("ERROR on connection MongoDB:", err);
+      return;
+    }
+
     console.log("MongoDB connection succeed");
-    module.exports = client;
-    const app = require("./app");
-     const server = http.createServer(app);
-     let PORT = 3001;
-     server.listen(PORT, function () {
-     console.log(
-      `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
-  );
-}); 
-   } 
-}); 
 
+    const db = client.db("Reja");
 
+    const app = require("./app")(db);
 
+    const server = http.createServer(app);
 
+    const PORT = 3000;
 
-
-
-
+    server.listen(PORT, () => {
+      console.log(
+        `The server is running successfully on port: ${PORT}, http://localhost:${PORT}`
+      );
+    });
+  }
+);
