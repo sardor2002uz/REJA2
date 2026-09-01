c669f41aefb96fdec55b0ce282b578666ea36f29
sardor <sardor020516@gmail.com>
2026-09-01 14:34:43 +0900

    feat: CRUD operatsiyalari

--- app.js
+++ app.js
@@ -30,17 +30,40 @@ app.set("view engine", "ejs");
 // 4 Routing code
 
 app.post("/create-item", (req, res) => {
-  console.log(req);
-  res.json({ test: "success" });
-});
+  console.log("user entered /create-item");
+  const new_reja = req.body.reja;
+  db.collection("plans").insertOne({reja: new_reja}, (err, data) => {
+   if(err) {
+    console.log(err);
+    res.end("something went wrong");
+   } else {
+    res.end("successfully added")
+   }
+  });
+  });
 
 // app.get("/author", (req, res) => {
 //   res.render("author", { user: user });
 // });
 
 app.get("/", function (req, res) {
-  res.render("reja");
+  console.log("user entered /");
+  db.collection("plans")
+  .find()
+  .toArray((err,data) => {
+    if (err) {
+      console.log(err);
+      res.end("something went wrong");
+    } else {
+      res.render("reja", { items: data} );
+      
+
+    }
+      
+  });
+    
 });
+  
 
 // app.get("/", function (req, res) {
 //   res.end(`<h1>HELLO WORLD</h1>`);
